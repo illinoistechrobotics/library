d0df91b3d3d6cfaac2f416a4a8ed288b9cf141c5
unknown <abirylo@.(none)>
2011-11-05 22:11:17 -0500

    Made small modifications to make it computer and robot compatible

--- ITR_Generic/GenericComputer/genericRobot/Profile.java
+++ ITR_Generic/GenericComputer/genericRobot/Profile.java
@@ -1,4 +1,3 @@
-package genericRobot;
 //    Copyright (C) 2011  Illinois Institute of Technology Robotics
 //	  <robotics@iit.edu>
 //
@@ -16,6 +15,7 @@ package genericRobot;
 //    with this program; if not, write to the Free Software Foundation, Inc.,
 //    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+package genericRobot;
 
 
 import java.util.*;
